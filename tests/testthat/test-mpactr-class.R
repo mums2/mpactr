4c7267de288263f18bd6b6a6bb7329e66561f87e
amason30 <masonar@umich.edu>
2024-06-06 15:54:07 -0400

    move mpactr class methods tests to their own script

--- tests/testthat/test-mpactr-class.R
+++ tests/testthat/test-mpactr-class.R
@@ -8,13 +8,5 @@ test_that("mpactr class initialize works correctly", {
   expect_error(mpactr$new(peak_table_path = 2, meta_data_path = 5), NULL)
 })
 
-test_that("mpactr class setup works properly", {
-  mpactr_class <- mpactr$new(here::here("tests/exttestdata/102623 peaktable coculture simple.csv"),
-      here::here("tests/exttestdata/102623_metadata_correct.csv"))
-  mpactr_class$setup()
-  
-  expect_true(all(c("kmd", "mz", "rt") %in% colnames(mpactr_class$peak_table)))
-  expect_equal(nrow(mpactr_class$peak_table), 1303)
-})
 
 
