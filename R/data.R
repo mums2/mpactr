f7fe3e7c02b771c9793b6cbdde234146a61be445
amason30 <masonar@umich.edu>
2024-06-10 16:17:56 -0400

    add dataset for examples

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' LC-MS/MS sample data
+#'
+#' A `mpactr` R6 class object of contining a feature table and associated sample
+#' metadata.
+#'
+#' @format ## `ColcultureData`
+#' A `mpactr` with 2 attributes:
+#' \describe{
+#'  \item{peak_table}{A feature table of class `data.table`}
+#'  \item{meta_data}{A `data.table` with associated sample metadata}
+#'  }
+#' @noRd
+#' @export 
+"CocultureData"
