8e323df117cba25c1b01a2db82efc04639287eb2
YoungKrug <31873199+YoungKrug@users.noreply.github.com>
2024-06-28 16:32:15 -0400

    Modified the constructor to allow for data.frames and data.tables to be passed through.

--- R/mpactr-class.R
+++ R/mpactr-class.R
@@ -3,8 +3,16 @@ mpactr <- R6Class("mpactr", public = list(
 
   # Constructor
   initialize = function(peak_table_path, meta_data_path) {
-    private$peak_table = data.table(readr::read_csv(peak_table_path, skip = 2, show_col_types = FALSE))
-    private$meta_data = data.table(readr::read_csv(meta_data_path, show_col_types = FALSE))
+    if((any(class(peak_table_path) %in% c("data.table", "data.frame"))) &&
+    (any(class(meta_data_path) %in% c("data.table", "data.frame"))))
+    {
+      private$peak_table = data.table(peak_table_path)
+      private$meta_data = data.table(meta_data_path)
+    }
+    else{
+      private$peak_table = data.table(readr::read_csv(peak_table_path, skip = 2, show_col_types = FALSE))
+      private$meta_data = data.table(readr::read_csv(meta_data_path, show_col_types = FALSE))
+    }
     stopifnot(any(class(private$peak_table) == "data.table"))
     stopifnot(any(class(private$meta_data) == "data.table"))
   },
