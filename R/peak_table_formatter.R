5dc031dddec4c96de24d6a745274d9fd8436cc8f
YoungKrug <31873199+YoungKrug@users.noreply.github.com>
2024-07-19 15:28:30 -0400

    Fix for formatter issues!

--- R/peak_table_formatter.R
+++ R/peak_table_formatter.R
@@ -25,13 +25,12 @@ format_by_type <- function(peak_table_path, type_of_peak_table, sample_names = N
 }
 
 
-
 progenesis_formatter <- function(peak_table){
-
 
     peak_table <- data.table(readr::read_csv(peak_table, skip = 2, show_col_types = FALSE))
     raw_peak_table <- peak_table
-    setnames(peak_table, c("m/z", "Retention time (min)"), c("mz", "rt"))
+    with(peak_table, setnames(peak_table, c("m/z", "Retention time (min)"), c("mz", "rt")))
+   # setnames(peak_table, c("m/z", "Retention time (min)"), c("mz", "rt"))
     
     return(list("peak_table" = peak_table,
                 "raw_table" = raw_peak_table))
@@ -45,24 +44,25 @@ mz_mine_formatter <- function(peak_table){
     return(list("peak_table" = peak_table,
                 "raw_table" = raw_peak_table))
 }
-# metaboscape_formatter <- function(peak_table, sample_names){
-#     peak_table <- data.table(readr::read_csv(peak_table, show_col_types = FALSE))
+metaboscape_formatter <- function(peak_table, sample_names){
+    peak_table <- data.table(readr::read_csv(peak_table, show_col_types = FALSE))
+    peak_table_convert <- data.table::copy(peak_table)
+    adduct_data <- utils::read.csv(system.file("extdata/ion_masses", "DefinedIons.csv", package = "mpactR") )
+    peak_table_convert <- with(peak_table_convert, peak_table_convert[ , ion := gsub(".*\\[(.+)\\].*", "\\1",
+                                                                                       ADDUCT)][
+                                                        , charge_string := gsub(".*\\](.+)", "\\1", ADDUCT)][
+                                                            charge_string == "+", charge := 1][
+                                                                charge_string == "2+", charge := 2][
+                                                                    charge_string == "3+", charge := 3][
+                                                                        adduct_data, on = .(ion = IONS)][
+                                                                            , mz := (PEPMASS / charge) + MASS])
 
-#     adduct_data <- utils::read.csv(system.file("extdata/ion_masses", "DefinedIons.csv", package = "mpactR") )
-    
-#     peak_table_convert <- peak_table[ , ion := gsub(".*\\[(.+)\\].*", "\\1", ADDUCT)][ 
-#                                                         , charge_string := gsub(".*\\](.+)", "\\1", ADDUCT)][
-#                                                             charge_string == "+", charge := 1][
-#                                                                 charge_string == "2+", charge := 2][
-#                                                                     charge_string == "3+", charge := 3][
-#                                                                         adduct_data, on = .(ion = IONS)][
-#                                                                             , mz := (PEPMASS / charge) + MASS]
+    # setnames(peak_table_convert, c("FEATURE_ID", "RT"), c("Compound", "rt"))
+    with(peak_table_convert, setnames(peak_table_convert, c("FEATURE_ID", "RT"), c("Compound", "rt")))
+    peak_table_mpactr <- with(peak_table_convert, peak_table_convert[ , .SD, .SDcols = c("Compound", "mz", "rt",
+                                                                                        sample_names)])
 
-#     setnames(peak_table_convert, c("FEATURE_ID", "RT"), c("Compound", "rt"))
-    
-#     peak_table_mpactr <- peak_table_convert[ , .SD, .SDcols = c("Compound", "mz", "rt", sample_names)]
-
-#     return(list("peak_table" = peak_table_mpactr,
-#                 "raw_table" = peak_table_convert))
-# }
+    return(list("peak_table" = peak_table_mpactr,
+                "raw_table" = peak_table_convert))
+}
 
