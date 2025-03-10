95d18944a804a11673ac5eaf697226e7cb3c2408
Gregory Johnson <31873199+GregJohnsonJr@users.noreply.github.com>
2025-03-10 15:42:40 -0400

    Fix compound duplication (#36)
    * Fix for #35
    
    * Added Test data, and new test for the new unique_compounds function.

--- tests/testthat/test-import_data.R
+++ tests/testthat/test-import_data.R
@@ -34,3 +34,19 @@ test_that("import_data aborts when expected
               format = "Progenesis"
             ))
           })
+
+
+test_that("unique_compounds annotate duplicates properly", { 
+  df <- data.frame(Compound = c(1,2,3,1:3,4:7))
+  ls <- list(peak_table = df, raw_table = df)
+  uniqued_list <- unique_compounds(ls)
+
+  expect_true(length(unique(uniqued_list$peak_table$Compound)) ==
+    length(uniqued_list$peak_table$Compound))
+  expect_true(length(unique(uniqued_list$raw_table$Compound)) ==
+   length(uniqued_list$raw_table$Compound))
+
+  expect_false(length(unique(df$Compound)) == length(df$Compound))
+})
+
+         
