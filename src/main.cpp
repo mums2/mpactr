d265e1ca66d616208245670ca6506e35f91999b7
GregJohnsonJr <gregjohn456@gmail.com>
2025-03-10 16:11:27 -0400

    Starting to create the filter mispicked ions function

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,22 @@
 void Test() {
     Rcpp::Rcout << "Hello World\n";
 }
+
+
+void FilterMispickedIons(const Rcpp::DataFrame& peakTable, const double ringWin, const double isoWin,
+    const double trWin, const double maxIsoShift, const bool mergePeaks,
+    const std::string& mergeMethod) {
+    const Rcpp::NumericVector mzVector = peakTable["mz"];
+    const Rcpp::NumericVector rtVector = peakTable["rt"];
+    const Rcpp::NumericVector compoundVector = peakTable["compound"];
+    const auto rows = static_cast<size_t>(peakTable.nrows());
+
+    for(size_t i = 0; i < rows - 1; i++) {
+        
+    }
+
+
+
+
+
+}
